b7efecc97553c6d8221ba3ec9c87fb65401c01f3
Marc Hoffman <marc.m.hoffman@gmail.com>
2023-08-11 15:15:49 -0400

    use macro

--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,80 @@
 #![allow(unused)]
 use bitmatch::bitmatch;
 
-#[repr(u8)]
-#[derive(Debug)]
-enum Regno {
- X0,X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,X11,X12,X13,X14,X15,
- X16,X17,X18,X19,X20,X21,X22,X23,X24,X25,X26,X27,X28,X29,X30,X31
-}
-impl From<u8> for Regno {
-    fn from(x: u8) -> Regno {
-        unsafe { std::mem::transmute(x & 0b11111) }
+macro_rules! fenum {
+  ( $ty:ident = { $( $enum:ident ),* } 
+  ) => {
+    #[repr(u8)]
+    #[derive(Debug)]
+    enum $ty {
+      $( $enum, )*
     }
-}
-impl From<u32> for Regno {
-    fn from(x: u32) -> Regno {
-        unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b11111) }
+    impl From<u32> for $ty {
+        fn from(x: u32) -> $ty {
+           unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b11111) }
+        }
     }
+  };
+  
+  ( $ty:ident = { $( $enum:ident : $val:expr ),* } 
+  ) => {
+    #[repr(u8)]
+    #[derive(Debug)]
+    enum $ty {
+      $( $enum=$val, )*
+    }
+    impl From<u32> for $ty {
+        fn from(x: u32) -> $ty {
+           unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b11111) }
+        }
+    }
+  }
 }
 
-#[repr(u8)]
-#[derive(Debug)]
-enum Cbranch {
- Beq, Bne, E0, E1, Blt,Bge,Bltu,Bgeu
-}
-impl From<u32> for Cbranch {
-    fn from(x: u32) -> Cbranch {
-        unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b111) }
-    }
-}
+fenum!{Regno = {
+	     X0,X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,X11,X12,X13,X14,X15,
+	     X16,X17,X18,X19,X20,X21,X22,X23,X24,X25,X26,X27,X28,X29,X30,X31 }
+	     }
 
-#[repr(u8)]
-#[derive(Debug)]
-enum LoadOp {
- Lb, Lh, Lw, Lbu, Lbh
-}
-impl From<u32> for LoadOp {
-    fn from(x: u32) -> LoadOp {
-        unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b111) }
-    }
-}
 
-#[repr(u8)]
-#[derive(Debug)]
-enum StoreOp {
- Sb, Sh, Sw,
-}
-impl From<u32> for StoreOp {
-    fn from(x: u32) -> StoreOp {
-        unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b111) }
-    }
-}
+fenum!{ Branch = { Beq:0, Bne:1,Blt:4,Bgt:5,Bltu:6,Bgeu:7 } }
 
-#[repr(u8)]
-#[derive(Debug)]
-enum AluIOp {
-   AddI, Slli, Slti, Sltiu,
-   Xori, Srli, Ori,  Andi,
-   E0, E1, E2, E3,
-   E4, Srai
-}
-impl From<u32> for AluIOp {
-    fn from(x: u32) -> AluIOp {
-        unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b1111) }
-    }
-}
+fenum!{ LoadOp = { Lb, Lh, Lw, Lbu, Lbh } }
 
-#[repr(u8)]
-#[derive(Debug)]
-enum AluOp {
-   Add,Sll,Slt,Sltu,
-   Xor,Srl,Or,And,
-   Sub,E0,E1,E2,
-   E3,Sra
-}
-impl From<u32> for AluOp {
-    fn from(x: u32) -> AluOp {
-        unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b1111) }
-    }
-}
+fenum!{ StoreOp = {  Sb, Sh, Sw } }
 
-#[repr(u8)]
-#[derive(Debug)]
-enum MulOp {
+fenum!{ AluIOp = {
+   AddI:0, Slli:1, Slti:2, Sltiu:3,
+   Xori:4, Srli:5, Ori:6,  Andi:7,
+   Srai:13
+} }
+
+fenum!{ AluOp = {
+   Add:0,Sll:1,Slt:2,Sltu:3,
+   Xor:4,Srl:5,Or:6,And:7,
+   Sub:8,Sra:13
+} }
+
+fenum!{ MulOp = {
      Mul,Mulh,Mulhsu,Mulhu,
      Div,Divu,Rem,Remu
-}
-impl From<u32> for MulOp {
-    fn from(x: u32) -> MulOp {
-        unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b111) }
-    }
-}
+} }
 
 
-#[repr(u8)]
-#[derive(Debug)]
-enum CsrOp {
-   Ecall,Csrrw,Csrrs,E0,
-   Csrrwi,Csrrsi,Csrrci,
-   Ebreak
-}
-impl From<u32> for CsrOp {
-    fn from(x: u32) -> CsrOp {
-        unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b1111) }
-    }
+fenum!{
+  CsrOp  = {
+   Ecall:0,Csrrw:1,Csrrs:2,
+   Csrrwi:3,Csrrsi:4,Csrrci:5,
+   Ebreak:6
+  }
 }
 
-
-#[repr(u8)]
-#[derive(Debug)]
-enum AmoOp {
-Amoaddw,Amoswapw,Lrw,Scw,
-Amoxorw,E5,E6,E7,
-Amoorw,E9,E10,E11,
-Amoandw,E13,E14,E15,
-Amominw,E17,E18,E19,
-Amomaxw,E21,E22,E23,
-Amominuw,E25,E26,E27,
-Amonmaxuw
-}
-impl From<u32> for AmoOp {
-    fn from(x: u32) -> AmoOp {
-        unsafe { std::mem::transmute(u8::try_from(x).unwrap() & 0b11111) }
-    }
+fenum!{
+   AmoOp = {
+     Amoaddw:0,Amoswapw:1,Lrw:2,Scw:3,
+     Amoxorw:4,Amoorw:8,Amoandw:12,Amominw:16,
+     Amomaxw:20,Amominuw:24,Amonmaxuw:28
+   }
 }
 
 enum RegImm {
@@ -136,7 +89,7 @@ enum RiscvOP {
   Jal(u32),
   Jalr(Regno,u32),
   Fencei,
-  Branch(Cbranch,Regno,Regno,u32),
+  Branch(Branch,Regno,Regno,u32),
   Load(LoadOp,Regno,Regno,u32),
   Store(StoreOp,Regno,Regno,u32),
   AluI(AluIOp,Regno,Regno,u32),
@@ -177,10 +130,14 @@ fn decode(inst: u32) -> RiscvOP {
 
 #[test]
 fn t0 () {
-    let x: u32 = 0x80000537;
-    let y = format!("{:?}", decode(x));
-    //writeln!("{}",y);
-    assert_eq!(y, "Lui(524288)");
+    assert_eq!("Lui(524288)",format!("{:?}",decode(0x80000537)));
+    assert_eq!("Branch(Bne, X15, X0, 4089)",format!("{:?}",decode( 0xfe079ce3)));
+    assert_eq!("Load(Lw, X13, X14, 4088)",format!("{:?}",decode(0xff872683)));
+    assert_eq!("Store(Sw, X0, X14, 15)",format!("{:?}",decode(0x00f72023)));
+    assert_eq!("AluI(AddI, X2, X2, 16)",format!("{:?}",decode(0x01010113)));
+    assert_eq!("Alu(Sub, X18, X18, X13)",format!("{:?}",decode(0x40d90933)));
+    assert_eq!("Csr(Csrrw, X0, X8, 155)",format!("{:?}",decode(0x13641073)));
+    assert_eq!("Mul(Divu, X8, X8, X18)",format!("{:?}",decode(0x03245433)));
 }
 
 fn main() {
